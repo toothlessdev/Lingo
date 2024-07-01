b2784880bcef3bfd0660e4b965b72d6fa8c0ab72
toothlessdev <toothless042@gmail.com>
2024-07-02 03:04:40 +0900

    feat: axios, base response configuration

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "",
+    baseURL: `http://${process.env.NEXT_PUBLIC_API_BASE_URL}/lingo-be/api`,
 });
 
 api.interceptors.request.use((config) => {
