d13c92201caee5e1b1641f98d71545c551f9bc2c
toothlessdev <toothless042@gmail.com>
2024-07-08 04:33:44 +0900

    feat: History, HistoryList 컴포넌트 구현

---
+++ src/components/display/HistoryList.tsx
@@ -0,0 +1,46 @@
+import { TrashIcon } from "@/assets/TrashIcon";
+import { Button } from "../form/Button";
+import { Textarea } from "../form/TextArea";
+import { History } from "./History";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+import { useCallback } from "react";
+import { logActions } from "@/store/log.slice";
+
+export const HistoryList = () => {
+    const dispatch = useDispatch();
+    const { logs } = useSelector((state: RootState) => state.log);
+
+    const clearHistory = useCallback(() => {
+        console.log("clear");
+        dispatch(logActions.clearLog());
+    }, [dispatch]);
+
+    return (
+        <div className="my-5 bg-gray-50 rounded-lg flex flex-col">
+            <div className="flex items-center justify-between mb-4">
+                <h2 className="text-lg font-medium">Translation History</h2>
+                <Button variant="ghost" size="icon" onClick={clearHistory}>
+                    <TrashIcon className="w-5 h-5" />
+                    <span className="sr-only">Clear history</span>
+                </Button>
+            </div>
+            <div className="space-y-4 overflow-auto flex-1">
+                {logs &&
+                    logs.map((log, index) => {
+                        return (
+                            <History
+                                key={`${index}-${log.time}`}
+                                translationModel={log.translationModel}
+                                srcLang={log.sourceLang}
+                                destLang={log.targetLang}
+                                targetSentence={log.targetSentence}
+                                translatedSentence={log.translatedSentence}
+                                time={log.time}
+                            />
+                        );
+                    })}
+            </div>
+        </div>
+    );
+};
