2555342a479343ee2e620a93c3694047980fb89a
toothlessdev <toothless042@gmail.com>
2024-07-08 04:34:41 +0900

    feat: QueryClientProvider 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ import "@/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import { persistor, store } from "@/store/store";
 import { PersistGate } from "redux-persist/lib/integration/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/config/query";
 
 export default function App({ Component, pageProps }: AppProps) {
     const router = useRouter();
@@ -17,19 +19,21 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <AnimatePresence mode="wait">
-                    <NavBar />
-                    <Toast />
-                    <motion.main
-                        key={router.route}
-                        initial={{ opacity: 0, x: -200 }}
-                        animate={{ opacity: 1, x: 0 }}
-                        exit={{ opacity: 0, x: 200 }}
-                        transition={{ duration: 0.5 }}
-                    >
-                        <Component {...pageProps} />
-                    </motion.main>
-                </AnimatePresence>
+                <QueryClientProvider client={queryClient}>
+                    <AnimatePresence mode="wait">
+                        <NavBar />
+                        <Toast />
+                        <motion.main
+                            key={router.route}
+                            initial={{ opacity: 0, x: -200 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0, x: 200 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            <Component {...pageProps} />
+                        </motion.main>
+                    </AnimatePresence>
+                </QueryClientProvider>
             </PersistGate>
         </Provider>
     );
