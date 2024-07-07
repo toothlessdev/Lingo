fa998fc15fdca764403e064b167e83928af7b985
toothlessdev <toothless042@gmail.com>
2024-07-08 04:34:26 +0900

    style: Ghost Button 스타일 수정

--- src/components/form/Button.tsx
+++ src/components/form/Button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
                 destructive: "bg-red-500 text-gray-50 hover:bg-red-500/90",
                 outline: "border text-gray-900 border-gray-200 bg-white hover:bg-gray-100 hover:text-gray-900",
                 secondary: "bg-gray-100 text-gray-900 hover:bg-gray-100/80",
-                ghost: "hover:bg-gray-100 hover:text-gray-900",
+                ghost: "hover:bg-yellow-100",
                 link: "text-gray-900 underline-offset-4 hover:underline",
             },
             size: {
